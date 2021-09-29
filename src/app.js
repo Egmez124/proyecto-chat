07bfafe613863f6c2763e84bd72968f94548ed00
Edgar Gregorio <egmez124@gmail.com>
2021-09-29 16:42:04 -0600

    agregando auth

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const userRoutes = require('./routes/users.routes');
 const converstionsRoutes = require("./routes/conversations.routes")
 const messagesRoutes = require("./routes/messages.routes");
 const participantsRoutes = require("./routes/participants.routes");
+const authRoutes = require("./routes/auth.routes");
 const app = express();
 
 app.use(express.json());
@@ -10,5 +11,5 @@ app.use('/api/v1/', userRoutes);
 app.use('/api/v1/', converstionsRoutes);
 app.use("/api/v1/", messagesRoutes);
 app.use("/api/v1/", participantsRoutes);
-
+app.use("/api/v1/", authRoutes);
 module.exports = app;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,9 @@
 const Router = require("express");
 const {getAllUsers, getUserById, createUser, updateUser, deleteUser} = require('../controllers/users.controllers')
+const {validateToken} = require("../middlewares/auth.middleware");
 const router = Router();
 
-router.get('/users', getAllUsers);
+router.get('/users', validateToken, getAllUsers);
 router.get('/users/:id', getUserById);
 router.post('/users', createUser);
 router.put('/users/:id', updateUser);
